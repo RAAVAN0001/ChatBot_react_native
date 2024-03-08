3cf2bf18dd298d7a25a614630c026389e2ad5043
raavan0001 <raavaannn@gmail.com>
2024-03-08 20:29:20 +0530

    Add new launcher icons and launcher background color

--- src/ChatScreen.tsx
+++ src/ChatScreen.tsx
@@ -1,163 +1,207 @@
-import { View, Text, Button, TextInput, StyleSheet, ScrollView, Pressable } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, ScrollView, Pressable, useColorScheme } from 'react-native';
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold, } from "@google/generative-ai";
 import Loading from './components/Loading';
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-import { trigger } from "react-native-haptic-feedback";
 import Markdown from 'react-native-markdown-display';
-import { API_KEY } from '../secret';
+import { trigger } from 'react-native-haptic-feedback';
 
+const MODEL_NAME = "gemini-1.0-pro";
+
+type ConversationItem = {
+  user: string;
+  ai: string;
+};
 
 const ChatScreen = () => {
-
-  const [result, setResult] = useState('Hey whats up!')
-  const [input, setInput] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
-
-
-
+  const [input, setInput] = useState('');
+  const [conversation, setConversation] = useState<ConversationItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<boolean>(false);
+  const [errorMsg, setErrorMsg] = useState<string>('');
   const options = {
     enableVibrateFallback: true,
     ignoreAndroidSystemSettings: false,
   };
 
+  const colorScheme = useColorScheme();
+  const backgroundColor = colorScheme === 'dark' ? 'black' : 'white';
+  const textColor = colorScheme === 'dark' ? 'white' : 'black';
 
-  function handleClick() {
-    console.log('Button Clicked')
-    if (input !== "") {
-      setIsLoading(true)
-      const genAI = new GoogleGenerativeAI(API_KEY);
+  const handleSendMessage = async (message: string) => {
+    const genAI = new GoogleGenerativeAI('AIzaSyCxI9SjzCYUhUWyjt-qAaVl4pmUrIHaFjM');
+    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
-      async function run() {
+    const generationConfig = {
+      temperature: 0.9,
+      topK: 1,
+      topP: 1,
+      maxOutputTokens: 2048,
+    };
+    const safetySettings = [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+      },
+    ];
 
-        try {
-          setInput('')
-          const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-          const prompt = input; // use the input value as the prompt
-          const result = await model.generateContent(prompt);
-          if (result) {
-            const response = await result.response;
-            const text = response.text();
-            setIsLoading(false)
-            setResult(text)
-            trigger("impactHeavy", options);
-            console.log(text);
-          } else {
-            setResult('something went wrong')
-          }
-        } catch (error) {
-          console.log(error)
-        }
+    const chat = model.startChat({
+      generationConfig,
+      safetySettings,
+      history: [
+        {
+          role: "user",
+          parts: [{ text: "hi" }],
+        },
+        {
+          role: "model",
+          parts: [{ text: "Hey there! How can I assist you today?" }],
+        },
+      ],
+    });
+
+    try {
+      if (message.trim() !== '') {
+        setError(false)
+        setIsLoading(true);
+        setInput('');
+        const result = await chat.sendMessage(message);
+        const response = result.response;
+        const text = response.text;
+
+        console.log(response.text());
+        setConversation((prevConversation: ConversationItem[]) => [
+          ...prevConversation,
+          { user: message, ai: text() }
+        ]);
+        trigger("impactHeavy", options);
+        setIsLoading(false);
+      } else {
+        console.log('please enter prompt');
       }
-      run();
-    } else {
-      setResult('Kindly add prompt')
+    } catch (error) {
+      // console.error("Error:--", error);
+      setError(true)
+      setIsLoading(false);
+      // setErrorMsg(error);
+      setErrorMsg('Your message violates our guidelines and cannot be processed. Please ensure it follows our community standards.');
     }
-  }
+  };
+
   const markdownStyles = {
     text: {
       color: 'white',
-      fontSize: 20,
-      marginTop: 10,
+      fontSize: 21,
+      marginTop: 15,
+      marginBottom: 15,
+      // backgroundColor:backgroundColor
     },
-    // Add more styles for other Markdown elements if needed
+  };
+
+  const renderConversation = () => {
+    return conversation.map((message, index) => (
+      <View key={index}>
+        <Text style={[styles.user]}>User: {message.user}</Text>
+        {/* <Text style={styles.ai}>AI: {message.ai}</Text> */}
+        <Markdown style={markdownStyles}>{message.ai}</Markdown>
+
+      </View>
+    ));
   };
 
   return (
     <View style={styles.container}>
-      {/* <Text style={{ color: '#009688', fontSize: 20, marginTop: 10 }}> AI : </Text> */}
-      {isLoading ? <Loading /> :
-
-
-        <ScrollView contentContainerStyle={styles.scrollViewContainer}>
-          <View style={styles.markdownContainer}>
-
-            <Markdown style={markdownStyles}>{result}</Markdown>
-
-          </View>
-        </ScrollView>
-
-
-
-
-
-      }
+      <ScrollView>
+        <View style={styles.conversationContainer}>
+          {renderConversation()}
+          {isLoading && <Loading />}
+          {error && <Text style={styles.errorText}>{errorMsg}</Text>}
+        </View>
+      </ScrollView>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
-          onChangeText={text => setInput(text)}
+          autoFocus
+          onChangeText={(text) => setInput(text)}
+          onSubmitEditing={() => handleSendMessage(input)}
           value={input}
-          multiline={true}
-          placeholder="What's in your mind..."
+          placeholder="Type your message..."
           placeholderTextColor={'gray'}
-          autoFocus={true}
+          multiline={true}
         />
-
-        <Pressable style={styles.buttonContainer} onPress={handleClick}>
+        <Pressable style={styles.buttonContainer} onPress={() => handleSendMessage(input)}>
           <Text style={styles.buttonText}>Send</Text>
         </Pressable>
-
       </View>
     </View>
-  )
-}
-
-export default ChatScreen
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
     backgroundColor: '#1e1e1e',
-    padding: 15
+    padding: 15,
   },
-  scrollViewContainer: {
-    flexGrow: 1,
-  },
-  markdownContainer: {
+  conversationContainer: {
     flex: 1,
-    padding: 10,
-  },
-  rplytxt: {
-    color: 'white',
-    fontSize: 20,
-    marginTop: 10,
-    // Ensure that the text wraps within its container
-    flexWrap: 'wrap',
+    marginBottom: 20,
   },
   inputContainer: {
-    marginBottom: 10,
     flexDirection: 'row',
     alignItems: 'center',
-    width: '100%',
-    gap: 5
+    marginBottom: 10,
   },
   input: {
-    height: 60,
+    flex: 1,
     borderColor: 'gray',
     borderWidth: 1,
-    paddingLeft: 10,
-    margin: 10,
+    paddingHorizontal: 10,
     color: 'white',
-    width: '70%',
+    fontSize: 20,
     borderRadius: 10,
-    fontSize: 20
+    marginRight: 10,
+    marginVertical: 20,
+    minHeight: 50,
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 20,
+    marginTop: 5,
+    fontWeight: '500'
   },
   buttonContainer: {
-    elevation: 8,
     backgroundColor: "#009688",
     borderRadius: 10,
     paddingVertical: 10,
     paddingHorizontal: 12,
-    justifyContent: "center",
-    height: 55,
-    width: '20%'
   },
   buttonText: {
     fontSize: 18,
     color: "#fff",
     fontWeight: "bold",
-    alignSelf: "center",
-    textTransform: "uppercase"
-  }
+    textTransform: "uppercase",
+  },
+  user: {
+    color: '#009688',
+    fontSize: 16,
+  },
+  ai: {
+    color: 'white',
+    fontSize: 16,
+    marginTop: 5,
+  },
 });
 
+export default ChatScreen;
